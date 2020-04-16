da2d9076cff0dd9c4d68fc279ec0f32dcbb6f26a
Robinclear <robin.peter.vkm@gmail.com>
2020-04-16 20:47:56 -0300

    Set up completed

--- src/com/norha/spring/Offers.java
+++ src/com/norha/spring/Offers.java
@@ -1,5 +1,14 @@
 package com.norha.spring;
 
 public class Offers {
+	private String offer = new String("No Current Offers");
+
+	public String getOffer() {
+		return offer;
+	}
+
+	public void setOffer(String offer) {
+		this.offer = offer;
+	}
 	
 }
--- src/com/norha/spring/User.java
+++ src/com/norha/spring/User.java
@@ -6,7 +6,8 @@ public class User {
 	public static void main(String[] args) {
 		
 		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("BeanScope.xml");
-		
+		Offers offers = (Offers) context.getBean("offers");
+		System.out.println(offers.getOffer());
 		context.close();
 	}
 }
