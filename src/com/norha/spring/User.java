91b2f992193a1d9b4631063ccc2eb79fe9ef6b33
Robinclear <robin.peter.vkm@gmail.com>
2020-04-16 20:42:17 -0300

    Second

--- src/com/norha/spring/User.java
+++ src/com/norha/spring/User.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class User {
 	public static void main(String[] args) {
 		
-		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("Beans.xml");
+		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("BeanScope.xml");
 		
 		context.close();
 	}
