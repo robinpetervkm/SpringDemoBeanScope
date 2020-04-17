77499c764568283a3815ecfbfc2d95db57048dff
Robinclear <robin.peter.vkm@gmail.com>
2020-04-16 21:01:17 -0300

    Singleton scope
    by default

--- src/com/norha/spring/User.java
+++ src/com/norha/spring/User.java
@@ -7,7 +7,18 @@ public class User {
 		
 		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("BeanScope.xml");
 		Offers offers = (Offers) context.getBean("offers");
-		System.out.println(offers.getOffer());
+		System.out.println("offers : "+offers.getOffer());
+		offers.setOffer("GET 10% Discount");
+		System.out.println("offers : "+offers.getOffer());
+		
+		Offers offers1 = (Offers) context.getBean("offers");
+		System.out.println("offers1 : "+offers1.getOffer());
+		
+		
+		
+		System.out.println(offers);
+		
+		System.out.println(offers1);
 		context.close();
 	}
 }
